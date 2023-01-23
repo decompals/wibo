b02a5b9d367aae67bfd179caa11be3316819913a
Luke Street <luke.street@encounterpc.com>
2023-01-23 10:25:36 -0500

    Update lmgr.cpp

--- dll/lmgr.cpp
+++ dll/lmgr.cpp
@@ -1,13 +1,13 @@
 #include "common.h"
 
 namespace lmgr {
-	int lp_checkout(int a, int b, const char* c, const char* d, int e, const char* f, int* out) {
+	int WIN_ENTRY lp_checkout(int a, int b, const char* c, const char* d, int e, const char* f, int* out) {
 		DEBUG_LOG("lp_checkout %d %d %s %s %d %s\n", a, b, c, d, e, f);
 		*out = 1234;
 		return 0;
 	}
 
-	int lp_checkin() {
+	int WIN_ENTRY lp_checkin() {
 		DEBUG_LOG("lp_checkin\n");
 		return 0;
 	}

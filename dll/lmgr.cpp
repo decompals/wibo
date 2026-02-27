1c724df8af5c865a483282b73e75ce72006f9458
Palapeli <26661008+mkwcat@users.noreply.github.com>
2026-02-26 20:38:11 -0500

    lmgr: Stub more MWCC license manager functions
    Required for compatibility with the "NXP 'CodeWarrior Special Edition' for MPC55xx/MPC56xx v2.10"

--- dll/lmgr.cpp
+++ dll/lmgr.cpp
@@ -6,6 +6,36 @@
 
 namespace lmgr {
 
+int CDECL lc_checkout() {
+	HOST_CONTEXT_GUARD();
+	DEBUG_LOG("lc_checkout()\n");
+	return 0;
+}
+
+int CDECL lc_set_attr() {
+	HOST_CONTEXT_GUARD();
+	DEBUG_LOG("lc_set_attr()\n");
+	return 0;
+}
+
+int CDECL lc_new_job() {
+	HOST_CONTEXT_GUARD();
+	DEBUG_LOG("lc_new_job()\n");
+	return 0;
+}
+
+int CDECL lc_free_job() {
+	HOST_CONTEXT_GUARD();
+	DEBUG_LOG("lc_free_job()\n");
+	return 0;
+}
+
+int CDECL lc_checkin() {
+	HOST_CONTEXT_GUARD();
+	DEBUG_LOG("lc_checkin()\n");
+	return 0;
+}
+
 int CDECL lp_checkout(int a, int b, LPCSTR c, LPCSTR d, int e, LPCSTR f, int *out) {
 	HOST_CONTEXT_GUARD();
 	DEBUG_LOG("lp_checkout(%d, %d, %s, %s, %d, %s)\n", a, b, c, d, e, f);
@@ -25,10 +55,20 @@ int CDECL lp_checkin() {
 
 static void *resolveByOrdinal(uint16_t ordinal) {
 	switch (ordinal) {
+	case 33:
+		return (void *)thunk_lmgr_lc_checkin;
+	case 34:
+		return (void *)thunk_lmgr_lc_checkout;
+	case 43:
+		return (void *)thunk_lmgr_lc_free_job;
+	case 61:
+		return (void *)thunk_lmgr_lc_set_attr;
 	case 189:
 		return (void *)thunk_lmgr_lp_checkin;
 	case 190:
 		return (void *)thunk_lmgr_lp_checkout;
+	case 249:
+		return (void *)thunk_lmgr_lc_new_job;
 	}
 	return 0;
 }
--- dll/lmgr.h
+++ dll/lmgr.h
@@ -4,6 +4,12 @@
 
 namespace lmgr {
 
+int CDECL lc_checkout();
+int CDECL lc_set_attr();
+int CDECL lc_new_job();
+int CDECL lc_free_job();
+int CDECL lc_checkin();
+
 int CDECL lp_checkout(int a, int b, LPCSTR c, LPCSTR d, int e, LPCSTR f, int *out);
 int CDECL lp_checkin();
 
